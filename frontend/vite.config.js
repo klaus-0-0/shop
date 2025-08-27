814b21ea4e9444f4fcdf6a511a799066c8d59c40
klaus-0-0 <jin0667666@gmail.com>
2025-08-27 05:54:31 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  server: {
+    port: Number(process.env.PORT) || 5173,
+    host: true,
+    allowedHosts: ['https://shop-ryri.onrender.com'], 
+  },
 })
