8df20314e850c5ddc8262a882bba3a13ccd39d50
klaus-0-0 <jin0667666@gmail.com>
2025-08-27 06:19:34 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('development'),
+  },
   plugins: [react()],
   server: {
     port: Number(process.env.PORT) || 5173,
     host: true,
-    allowedHosts: ['frontend-wr1l.onrender.com'], 
+    allowedHosts: ['frontend-wr1l.onrender.com'],
   },
 })
